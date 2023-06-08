630c9d3cce13182acacf739abdafdc6e1525c94c
jy0813 <busbug3@gmail.com>
2023-06-08 13:26:13 +0900

    feat: 백준 1316: 그룹 단어 체커

---
+++ 백준/Silver5/1316_그룹 단어 체커.js
@@ -0,0 +1,27 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let count = 0;
+
+function solution(data) {
+    let setData = new Set(data[0]);
+    for(let i = 0; i < data.length -1; i++) {
+        if(data[i] !== data[i + 1]) {
+            if(setData.has(data[i + 1])) {
+                return false;
+            }
+            else {
+                setData.add(data[i + 1]);
+            }
+        }
+    }
+    return true;
+}
+
+for(let i = 1; i <= n; i++) {
+    let data = input[i];
+    if(solution(data)) count += 1;
+}
+
+console.log(count);
