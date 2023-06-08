941a973e6349bb222ea33df381d1e53a16de0a9d
jy0813 <busbug3@gmail.com>
2023-06-08 23:44:36 +0900

    feat: 백준 11651: 좌표 정렬하기2

---
+++ 백준/Silver5/11651_좌표 정렬하기2.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+
+let n = Number(input[0]);
+let arr = []
+
+for(let i = 1; i <= n; i++) {
+    const [x,y] = input[i].split(' ').map(Number);
+    arr.push([x,y])
+}
+
+const sortedArr = arr.sort((a,b) => a[1] !== b[1] ? a[1] - b[1] : a[0] - b[0]);
+const answer = sortedArr.map((item) => item.join(' ')).join('\n');
+
+console.log(answer)
