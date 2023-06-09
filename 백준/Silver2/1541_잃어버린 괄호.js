505f344e83f221e6ba5831d804822ee2baa5bb4a
jy0813 <busbug3@gmail.com>
2023-06-09 12:52:09 +0900

    feat: 백준 1541: 잃어버린 괄호

---
+++ 백준/Silver2/1541_잃어버린 괄호.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let groups = input[0].split('-');
+
+let answer = 0;
+
+for(let i = 0; i < groups.length; i++) {
+    let cur = groups[i].split('+').map(Number).reduce((accu, curr) => accu + curr);
+    if(i === 0) {
+        answer += cur;
+    } else {
+        answer -= cur;
+    }
+}
+
+console.log(answer);
